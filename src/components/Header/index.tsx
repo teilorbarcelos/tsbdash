62d4bf2166415fde97169c8e8b67c49196badecb
Teilor Souza Barcelos <tsb-@live.com>
2021-12-17 22:13:53 -0300

    responsive Sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react"
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react"
+import { RiMenuLine } from "react-icons/ri"
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext"
 import { Logo } from "./Logo"
 import { NotificationsNav } from "./NotificationsNav"
 import { Profile } from "./Profile"
 import { SearchBox } from "./SearchBox"
 
 export default function Header() {
+  const { onOpen } = useSidebarDrawer()
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
@@ -21,6 +24,19 @@ export default function Header() {
       px="6"
       align="center"
     >
+
+      {
+        !isWideVersion &&
+        <IconButton
+          aria-label="Open Navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize={34}
+          variant="unstyled"
+          onClick={onOpen}
+          mr={2}
+        ></IconButton>
+      }
+
       <Logo />
 
       {
